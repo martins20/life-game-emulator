5105558905fd33a7d0845d472b9031d3604a6e35
martins20 <martins20.tech@gmail.com>
2022-04-21 01:23:11 -0300

    refactor: change PlayersRepository container name

--- src/modules/player/services/create-player.ts
+++ src/modules/player/services/create-player.ts
@@ -8,18 +8,18 @@ import { CreatePlayerDTO } from "../dtos/create-player";
 @injectable()
 export class CreatePlayerService {
   constructor(
-    @inject("PlayerRepository")
-    private playerRepository: PlayerRepositoryContract
+    @inject("PlayersRepository")
+    private playersRepository: PlayerRepositoryContract
   ) {}
 
   async execute(data: CreatePlayerDTO): Promise<Player> {
     const foundPlayerWithSameName =
-      await this.playerRepository.findByPlayerName(data.name);
+      await this.playersRepository.findByPlayerName(data.name);
 
     if (foundPlayerWithSameName)
       throw new PlayerErrors.PlayerAlreadyExistsError();
 
-    const player = await this.playerRepository.create(data);
+    const player = await this.playersRepository.create(data);
 
     return player;
   }
