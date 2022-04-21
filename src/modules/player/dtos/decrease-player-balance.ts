01f4494d7a1404cbd1389849740959dedb2444db
martins20 <martins20.tech@gmail.com>
2022-04-21 01:35:29 -0300

    feat: decrease and increase player's balance

--- src/modules/player/dtos/decrease-player-balance.ts
+++ src/modules/player/dtos/decrease-player-balance.ts
@@ -2,5 +2,5 @@ import { Player } from "../entities/Player";
 
 export interface DecreasePlayerBalanceDTO {
   player_id: Player["name"];
-  value: -Player["balance"];
+  value: Player["balance"];
 }
