103e76cf3cd6354c98d3d7bb929fc3b93a3b934f
martins20 <martins20.tech@gmail.com>
2022-04-25 15:40:50 -0300

    doc: create swagger documentation

--- src/shared/Server.ts
+++ src/shared/Server.ts
@@ -2,7 +2,11 @@ import "dotenv/config";
 import "reflect-metadata";
 import "express-async-errors";
 
+import SwaggerUI from "swagger-ui-express";
 import express, { Express } from "express";
+import cors from "cors";
+
+import SwaggerSetup from "@shared/documentation/swagger.json";
 
 import { apiRoutes } from "./infra/http/routes/index.routes";
 import HandleErrorsMiddleware from "./infra/http/middlewares/hanlde-errors";
@@ -28,7 +32,14 @@ class Server {
   }
 
   private middlewares(): void {
+    this.api.use(cors());
     this.api.use(express.json());
+
+    this.api.use(
+      "/documentation",
+      SwaggerUI.serve,
+      SwaggerUI.setup(SwaggerSetup)
+    );
   }
 
   private useRoutes(): void {
